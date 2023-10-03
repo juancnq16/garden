afb72de9ae2a7e092acbf8499a52af70012ab62d
MightyXyah420 <juancnq16@gmail.com>
2023-10-03 14:18:22 -0500

    Initialized maven project

---
+++ src/main/java/com/juan/garden/GardenAppApplication.java
@@ -0,0 +1,13 @@
+package com.juan.garden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GardenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GardenAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/juan/garden/GardenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juan.garden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GardenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
